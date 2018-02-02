85b5d6467cf17d2e0eaf4cfce69329f1bb5040c4
kpdebree <kpdebree@gmail.com>
2018-02-01 21:17:07 -0800

    Created Mongoose Schemas

---
+++ models/leasecomps.js
@@ -0,0 +1,46 @@
+// require mongoose
+var mongoose = require("mongoose");
+
+var Schema = mongoose.Schema;
+
+var leaseSchema = new Schema({
+	address: {
+		type: String,
+		required: true
+	},
+	city: {
+		type: String,
+		required: true
+	},
+	state: {
+		type: String,
+		required: true
+	},
+	latitude: {
+		type: Number,
+		required: true
+	},
+	longitude: {
+		type: Number,
+		required: true
+	},
+	propertyName: {
+		type: String,
+	},
+	squarefeet: {
+		type: Number,
+		required: true
+	},
+	type: {
+		type: String,
+		required: true
+	},
+	askingrent: {
+		type: Number,
+		required: true
+	}
+});
+
+var LeaseComps = mongoose.model("LeaseComps", leaseSchema);
+
+module.exports = LeaseComps;
