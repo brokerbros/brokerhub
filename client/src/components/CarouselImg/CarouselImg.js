c1920a82830289076ec075c78bbbe913218d7408
Fausto Rendon <faustor001@gmail.com>
2018-02-06 21:13:03 -0800

    Revert "Property"

--- client/src/components/CarouselImg/CarouselImg.js
+++ client/src/components/CarouselImg/CarouselImg.js
@@ -3,8 +3,8 @@ import "./CarouselImg.css";
 
 const CarouselImg = (props) => {
     return (
-        <div className={props.active}>
-            <img className='d-block w-100 pic-restruc img' src={props.image} />
+        <div className={props.clicked}>
+            <img className='d-block w-100 pic-restruc' src={props.image} />
             <div className="carousel-caption d-none d-md-block">
                 <p className='textcolor'>{props.id}</p>
             </div>
