dc6b18be5fcff2963fc95b048b9e56e1d6eb11d4
Kevin de Bree <kpdebree@gmail.com>
2018-02-07 22:15:56 -0800

    Map is working. #Swag

--- client/src/components/pages/Search.js
+++ client/src/components/pages/Search.js
@@ -1,48 +1,20 @@
 import React, { Component } from "react";
 import ReactDOM from 'react-dom';
-import GoogleMapReact from 'google-map-react';
+import { MapContainer } from '../Map'
 import "./main.css";
 import "./map.css";
 import SideNav from "../SideNav/SideNav";
 
-// const Search = () =>
-//     <div className="container" id="map">
-//     <h1>Broker Bros, Bro</h1>
-//     <p>Insert Map Here</p>
-//   </div>;
-
-// export default Search;
-
-const Search = ({ text }) => <div>{text}</div>;
-
-class SimpleMap extends Component {
-	static defaultProps = {
-		center: {lat: 34.040363, lng: -118.248691},
-		zoom: 10
-	};
+class Search extends Component {
 
 	render() {
-		return (
-		<GoogleMapReact
-			defaultCenter={this.props.center}
-			defaultZoom={this.props.zoom}
-		>
-
-			<Search
-			 lat={34.040363}
-			 lng={-118.248691}
-			 text={'BrokerBros, Bro!'}
-			/>
-		</GoogleMapReact>
+		return ( 
+		<div className='container'>
+			<h1>Map Container</h1>
+			<MapContainer />
+		</div>
 		);
-	}
+	}	
 }
-
-// ReactDOM.render(
-// 	<div style={{width: '100%', height: '400px'}}>
-// 		<SimpleMap/>
-// 	</div>,
-// 	document.getElementById('root')
-// );
 
 export default Search;
