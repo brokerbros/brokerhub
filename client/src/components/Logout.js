8761704e8d9bcdf58ddb64ad5979df13f250329a
Evan Lu <evanlu7@gmail.com>
2018-02-08 20:14:10 -0800

    a

--- client/src/components/Logout.js
+++ client/src/components/Logout.js
@@ -14,15 +14,24 @@ class Logout extends Component {
   componentWillMount() {
     app.auth().signOut().then((user) => {
       this.setState({ redirect: true })
+      
     })
   }
 
+  redirectToMain() {
+    if (this.state.redirect === true) {
+      return <Redirect to={{pathname: '/'}} />;
+    } 
+  }
+
   render() {
-      return 
+      return(
       <div style={{ textAlign: "center", position: "absolute", top: "25%", left: "50%" }}>
         <h3>Logging Out</h3>
         <Spinner />
+        {this.redirectToMain()}
       </div>
+      );
     }
 }
 
