90b6f187f3f2dc1838075ae9e3ba17fb10d23c0c
kpdebree <kpdebree@gmail.com>
2018-02-08 16:02:56 -0800

    Fixed API.js file

--- client/src/components/Map/Map.js
+++ client/src/components/Map/Map.js
@@ -7,7 +7,8 @@ export class MapContainer extends React.Component {
 	render() {
 		const style = {
  			width: '100%',
-  			height: '100%'
+  			height: '100%',
+  			overflow: 'hidden'
 		}
 
 		if (!this.props.loaded) {
