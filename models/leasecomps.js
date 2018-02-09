3d40c4b9dc25a4bd79a5166f8b9257ba126bde31
Evan Lu <evanlu7@gmail.com>
2018-02-08 18:49:20 -0800

    respaced the db files

--- models/leasecomps.js
+++ models/leasecomps.js
@@ -4,51 +4,45 @@ var mongoose = require("mongoose");
 var Schema = mongoose.Schema;
 
 var leaseSchema = new Schema({
-	address: {
-	type: String,
-
-	},
-	city: {
-		type: String,
-		
-	},
-	state: {
-		type: String,
-		
-	},
-	latitude: {
-		type: Number,
-		required: true
-	},
-	longitude: {
-		type: Number,
-		required: true
-	},
-	propertyName: {
-		type: String,
-	},
-	askingrent: {
-		type: Number,
-		required: true
-	},
-		propertyName: {
-		type: String,
-	},
-	squarefeet: {
-		type: Number,
-	
-	},
-	type: {
-		type: String,
-		
-	},
-	saleprices: {
-		type: Number,
-		
-	},
-	listingBroker: {
-		type: String
-	}
+  address: {
+    type: String,
+  },
+  city: {
+    type: String,
+  },
+  state: {
+    type: String,
+  },
+  latitude: {
+    type: Number,
+    required: true
+  },
+  longitude: {
+    type: Number,
+    required: true
+  },
+  propertyName: {
+    type: String,
+  },
+  askingrent: {
+    type: Number,
+    required: true
+  },
+  propertyName: {
+    type: String,
+  },
+  squarefeet: {
+    type: Number,
+  },
+  type: {
+    type: String,
+  },
+  saleprices: {
+    type: Number,
+  },
+  listingBroker: {
+    type: String
+  }
 });
 
 var LeaseComps = mongoose.model("LeaseComps", leaseSchema);
--- models/salescomps.js
+++ models/salescomps.js
@@ -4,39 +4,39 @@ var mongoose = require("mongoose");
 var Schema = mongoose.Schema;
 
 var salesSchema = new Schema({
-	address: {
-		type: String,
-	},
-	city: {
-		type: String,
-	},
-	state: {
-		type: String,
-	},
-	latitude: {
-		type: Number,
-		required: true
-	},
-	longitude: {
-		type: Number,
-		required: true
-	},
-	propertyName: {
-		type: String,
-	},
-	squarefeet: {
-		type: Number,
-	},
-	type: {
-		type: String,
-	},
-	saleprices: {
-		type: Number,
-		required: true
-	},
-	listingBroker: {
-		type: String
-	}
+  address: {
+    type: String,
+  },
+  city: {
+    type: String,
+  },
+  state: {
+    type: String,
+  },
+  latitude: {
+    type: Number,
+    required: true
+  },
+  longitude: {
+    type: Number,
+    required: true
+  },
+  propertyName: {
+    type: String,
+  },
+  squarefeet: {
+    type: Number,
+  },
+  type: {
+    type: String,
+  },
+  saleprices: {
+    type: Number,
+    required: true
+  },
+  listingBroker: {
+    type: String
+  }
 });
 
 var SalesComps = mongoose.model("SalesComps", salesSchema);
